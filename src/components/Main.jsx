333ddd7b3b44b68e2213f252b3b1277723520a57
Juniiie <jrbuking@gmail.com>
2023-04-30 18:08:08 +1000

    Added YinYang + ABOUT + MYSKILLS PAGE

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
 import { PowerButton } from "../subComponents/PowerButton";
 import LogoComponent from "../subComponents/LogoComponent";
 import SocialIcons from "../subComponents/SocialIcons";
+import { YinYang } from "./AllSvgs";
 
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
@@ -26,6 +28,92 @@ const Container = styled.div`
   padding: 2rem;
 `;
 
+const Contact = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BLOG = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  transform: rotate(90deg) translate(-50%, -50%);
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const WORK = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  left: calc(1.3rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const ABOUT = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const SKILLS = styled(NavLink)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`;
+
+// YIN YANG
+const Center = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > :first-child {
+    animation: ${rotate} infinite 1.5s linear;
+  }
+
+  & > :last-child {
+    padding-top: 1rem;
+  }
+`;
+
 export const Main = () => {
   return (
     <MainContainer>
@@ -33,6 +121,36 @@ export const Main = () => {
         <PowerButton />
         <LogoComponent />
         <SocialIcons />
+
+        <Center>
+          <YinYang width={150} height={150} fill="currentColor" />
+          <span>click here</span>
+        </Center>
+
+        {/* MAKE THE EMAIL FUNCTION WORK */}
+        <Contact target="_blank" href="mailto:codebucks27@gmail.com">
+          <h2>Say hi...</h2>
+        </Contact>
+
+        {/* BLOG */}
+        <BLOG to="/blog">
+          <h2>Blog.</h2>
+        </BLOG>
+
+        {/* WORK */}
+        <WORK to="/work">
+          <h2>Work.</h2>
+        </WORK>
+
+        {/* BOTTOM BAR */}
+        <BottomBar>
+          <ABOUT to="/about">
+            <h2>About.</h2>
+          </ABOUT>
+          <SKILLS to="/skills">
+            <h2>My Skills.</h2>
+          </SKILLS>
+        </BottomBar>
       </Container>
     </MainContainer>
   );
